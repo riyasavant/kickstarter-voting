89110ce8419f777d6a70e54fd44fa7314df5ff54
Riya Savant <rieee2110@gmail.com>
2021-10-29 14:24:07 +0530

    Added compile / deploy scripts

---
+++ ethereum/deploy.js
@@ -0,0 +1,25 @@
+const HDWalletProvider = require("@truffle/hdwallet-provider");
+const Web3 = require("web3");
+const compiledFactory = require("./build/CampaignFactory.json");
+
+const provider = new HDWalletProvider(
+  "pen lumber couch inmate defy pupil neglect river mind sweet visa art",
+  "wss://rinkeby.infura.io/ws/v3/0f940dbce5a94f79878c957017f5c9a4"
+);
+const web3 = new Web3(provider);
+
+const deploy = async () => {
+  const accounts = await web3.eth.getAccounts();
+
+  console.log("Attempting to deploy from account", accounts[0]);
+
+  const result = await new web3.eth.Contract(
+    JSON.parse(compiledFactory.interface)
+  )
+    .deploy({ data: compiledFactory.bytecode })
+    .send({ gas: '1000000', gasPrice: '5000000000', from: accounts[0] });
+
+  console.log("Contract deployed to", result.options.address);
+  provider.engine.stop();
+};
+deploy();
---
+++ ethereum/factory.js
@@ -0,0 +1,9 @@
+import web3 from "./web3";
+import CampaignFactory from "./build/CampaignFactory.json";
+
+const instance = new web3.eth.Contract(
+    JSON.parse(CampaignFactory.interface),
+    '0xa8A452f4db23afe330C8a24Dd253040987B339dA'
+)
+
+export default instance;
